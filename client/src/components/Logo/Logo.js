27d8fb98104256057f7c6d46bbb7984618042dce
MaksymBohdan <m.bohdan@globit.com>
2019-11-17 12:18:03 +0200

    feat: client settings initialization

---
+++ client/src/components/Logo/Logo.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import unknownLogo from '../../assets/face.jpeg';
+
+const Logo = ({ user, width = '100', height = '100' }) => (
+  <img src={user ? user.src : unknownLogo} alt='face' width={width} height={height} />
+);
+
+export default Logo;
